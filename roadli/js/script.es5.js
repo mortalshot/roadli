1c0f8e6f3175b686499e7d7e234471afa8d9cb50
mortal <seregagl1996@gmail.com>
2021-03-27 16:12:08 +0300

    map section

--- roadli/js/script.es5.js
+++ roadli/js/script.es5.js
@@ -338,4 +338,46 @@ $(document).ready(function () {
       $("#" + elementID + " .tabs__triggers-item:first").click();
     }
   }
+
+  var animItems = document.querySelectorAll('._anim-items');
+
+  if (animItems.length > 0) {
+    var animOnScroll = function animOnScroll(params) {
+      for (var _index = 0; _index < animItems.length; _index++) {
+        var animItem = animItems[_index];
+        var animItemHeight = animItem.offsetHeight;
+        var animItemOffset = offset(animItem).top;
+        var animStart = 55; // Коэффициент старта анимации
+
+        var animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if (pageYOffset > animItemOffset - animItemPoint && pageYOffset < animItemOffset + animItemHeight) {
+          animItem.classList.add('_active');
+        } else {
+          if (!animItem.classList.contains('_anim-no-hide')) {
+            animItem.classList.remove('_active');
+          }
+        }
+      }
+    };
+
+    var offset = function offset(el) {
+      var rect = el.getBoundingClientRect(),
+          scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+          scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return {
+        top: rect.top + scrollTop,
+        left: rect.left + scrollLeft
+      };
+    };
+
+    window.addEventListener('scroll', animOnScroll);
+    setTimeout(function () {
+      animOnScroll();
+    }, 300);
+  }
 });
